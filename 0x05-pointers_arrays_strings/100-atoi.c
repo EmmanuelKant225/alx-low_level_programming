79b4bdf9f9fb12e903fa83bcf681ff506cb5dcde
Emmanuel Adigra <systemengineer225@yahoo.com>
2023-01-08 06:04:38 -0800

    Write a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,20 +8,17 @@
 int _atoi(char *s)
 {
 	int i = 0, res = 0, sign = 1;
-
-	// Verify the sign
 	if (s[0] == '-')
 	{
 		sign = -1;
 		i++;
 	}
 
-	// Convert the string to number
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		res = res * 10 + (s[i] - '0');
 		i++
 	}
 
-	return sign * res;
+	return sign * res
 }
